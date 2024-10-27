5654a77a3e4b6a574f83ae78bceabc0673261391
shaulascr <alya.roosyidah@gmail.com>
2024-10-27 19:35:10 +0700

    reset

--- app/src/main/java/com/alya/ecommerce_serang/ui/MainActivity.kt
+++ app/src/main/java/com/alya/ecommerce_serang/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.alya.ecommerce_serang
+package com.alya.ecommerce_serang.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.alya.ecommerce_serang.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
