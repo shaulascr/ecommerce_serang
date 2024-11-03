f50a230bb0ec450966fb45f6d9d85af255845362
shaulascr <alya.roosyidah@gmail.com>
2024-11-03 20:55:33 +0700

    Add Bottom Navigation

--- app/src/main/java/com/alya/ecommerce_serang/ui/MainActivity.kt
+++ app/src/main/java/com/alya/ecommerce_serang/ui/MainActivity.kt
@@ -6,10 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.alya.ecommerce_serang.R
+import com.alya.ecommerce_serang.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -17,5 +24,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
     }
 }
