854178c9e4b7ac0b5bd767336f651000256dfb91
shaulascr <alya.roosyidah@gmail.com>
2025-02-12 21:28:58 +0700

    add profile

--- app/src/main/java/com/alya/ecommerce_serang/data/api/retrofit/ApiService.kt
+++ app/src/main/java/com/alya/ecommerce_serang/data/api/retrofit/ApiService.kt
@@ -2,6 +2,7 @@ package com.alya.ecommerce_serang.data.api.retrofit
 
 import com.alya.ecommerce_serang.data.api.response.AllProductResponse
 import com.alya.ecommerce_serang.data.api.response.ProductResponse
+import com.alya.ecommerce_serang.data.api.response.StoreResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
@@ -19,4 +20,9 @@ interface ApiService {
         @Header("Authorization") token: String,
         @Path("id") productId: Int
     ): Call<ProductResponse>
+
+    @GET("mystore")
+    fun getStore (
+        @Header("Authorization") token: String = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzEsIm5hbWUiOiJhbHlhIiwiZW1haWwiOiJha3VuYmVsYWphci5hbHlhQGdtYWlsLmNvbSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNzM4NDg0OTc0LCJleHAiOjE3NDEwNzY5NzR9.0JyXJQ_6CKiZEi0gvk1gcn-0ILu3a9lOr3HqjhJXbBE"
+    ): Call<StoreResponse>
 }
