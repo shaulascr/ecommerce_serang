422cceaadba8507fa7d969daf5c86d352f5fed31
gracia <hotmauligracia@gmail.com>
2025-02-17 00:42:03 +0700

    update test

---
+++ app/src/main/java/com/alya/ecommerce_serang/ui/profile/TokoSayaActivity.kt
@@ -0,0 +1,21 @@
+package com.alya.ecommerce_serang.ui.profile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.alya.ecommerce_serang.R
+
+class TokoSayaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_toko_saya)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
