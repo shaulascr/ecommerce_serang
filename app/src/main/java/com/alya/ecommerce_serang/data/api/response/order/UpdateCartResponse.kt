0c128337aed3b6632608eba2a7d0c85dab7daee4
shaulascr <alya.roosyidah@gmail.com>
2025-04-09 15:47:30 +0700

    update address

---
+++ app/src/main/java/com/alya/ecommerce_serang/data/api/response/order/UpdateCartResponse.kt
@@ -0,0 +1,9 @@
+package com.alya.ecommerce_serang.data.api.response.order
+
+import com.google.gson.annotations.SerializedName
+
+data class UpdateCartResponse(
+
+	@field:SerializedName("message")
+	val message: String
+)
