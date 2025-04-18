3b0b2d602f41b82cb587d0c9b6d361abb3783c68
Gracia <hotmauligracia@gmail.com>
2025-04-19 02:04:40 +0700

    update product

--- app/src/main/java/com/alya/ecommerce_serang/ui/profile/mystore/product/ProductAdapter.kt
+++ app/src/main/java/com/alya/ecommerce_serang/ui/profile/mystore/product/ProductAdapter.kt
@@ -4,10 +4,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.PopupMenu
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.alya.ecommerce_serang.R
+import com.alya.ecommerce_serang.data.api.dto.Product
 import com.alya.ecommerce_serang.data.api.dto.ProductsItem
 import com.bumptech.glide.Glide
 
@@ -22,26 +26,36 @@ class ProductAdapter(
         private val tvProductPrice: TextView = itemView.findViewById(R.id.tv_product_price)
         private val tvProductStock: TextView = itemView.findViewById(R.id.tv_product_stock)
         private val tvProductStatus: TextView = itemView.findViewById(R.id.tv_product_status)
+        private val ivMenu: ImageView = itemView.findViewById(R.id.iv_menu)
 
         fun bind(product: ProductsItem) {
             tvProductName.text = product.name
             tvProductPrice.text = "Rp${product.price}"
             tvProductStock.text = "Stok: ${product.stock}"
-            tvProductStatus.text = product.status
 
-            // Change color depending on status
-            tvProductStatus.setTextColor(
-                ContextCompat.getColor(
-                    itemView.context,
-                    if (product.status.equals("active", true))
-                        R.color.darkblue_500 else R.color.black_500
-                )
-            )
+            if (product.status.equals("active",true)) {
+                tvProductStatus.text = "Aktif"
+                tvProductStatus.setTextColor(ContextCompat.getColor(itemView.context, R.color.darkblue_500))
+                tvProductStatus.background = ContextCompat.getDrawable(itemView.context, R.drawable.bg_product_active)
+            } else {
+                tvProductStatus.text = "Nonaktif"
+                tvProductStatus.setTextColor(ContextCompat.getColor(itemView.context, R.color.black_500))
+                tvProductStatus.background = ContextCompat.getDrawable(itemView.context, R.drawable.bg_product_inactive)
+            }
 
             Glide.with(itemView.context)
                 .load(product.image)
                 .placeholder(R.drawable.placeholder_image)
                 .into(ivProduct)
+
+            ivMenu.setOnClickListener {
+                // Show Bottom Sheet when menu is clicked
+                val bottomSheetFragment = ProductOptionsBottomSheetFragment(product)
+                bottomSheetFragment.show(
+                    (itemView.context as FragmentActivity).supportFragmentManager,
+                    bottomSheetFragment.tag
+                )
+            }
 
             itemView.setOnClickListener {
                 onItemClick(product)
